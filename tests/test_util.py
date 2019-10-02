325b8585945fdf53b13a28ec21fddbf9a7893ae1
lumbric <lumbric@gmail.com>
2019-10-02 15:24:57 +0200

    Add quantile() function which can deal with np.inf

--- tests/test_util.py
+++ tests/test_util.py
@@ -1,8 +1,20 @@
+import numpy as np
+import xarray as xr
 from wind_repower_usa.load_data import load_turbines
-from wind_repower_usa.util import turbine_locations
+from wind_repower_usa.util import turbine_locations, quantile
 
 
 def test_turbine_locations():
     turbines = load_turbines()
     locations = turbine_locations(turbines)
     assert locations.shape == (turbines.sizes['turbines'], 2)
+
+
+def test_quantile():
+    a = xr.DataArray(np.random.rand(33, 43, 23), dims=('a', 'b', 'c'))
+
+    assert a.quantile(0.23) == quantile(a, 0.23)
+    np.testing.assert_array_equal(a.quantile(0.23, dim='b'),
+                                  quantile(a, 0.23, dim='b'))
+
+    assert quantile(xr.DataArray([1, 2, 3, np.inf, np.inf, np.inf]), 0.5) == np.inf
--- wind_repower_usa/util.py
+++ wind_repower_usa/util.py
@@ -24,3 +24,25 @@ def turbine_locations(turbines):
 
 def edges_to_center(edges):
     return edges[:-1] + (edges[1] - edges[0])/2.
+
+
+def quantile(a, q, dim=None, **kwargs):
+    """Compute the qth quantile of the data along the specified dimension. This is just a wrapper
+    around `xr.DataArray.quantile()` which allows usage of `np.inf`. Not quite sure if this is
+    done in a numerical stable way.
+    """
+    if 'interpolation' in kwargs:
+        raise ValueError
+
+    lower = a.quantile(q, dim=dim, interpolation='lower', **kwargs)
+    higher = a.quantile(q, dim=dim, interpolation='higher', **kwargs)
+
+    size = a.sizes[dim] if dim is not None else a.size
+    fraction = np.fmod(q * (size - 1), 1)
+
+    if fraction == 0.:
+        return lower
+    elif fraction == 0.:
+        return higher
+    else:
+        return (1. - fraction) * lower + fraction * higher
